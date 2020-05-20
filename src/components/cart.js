69c164e521f48e97de42370397d9d34a8f39d59f
harkon666 <bryan.wicaksanaa@gmail.com>
2020-05-20 23:06:45 +0700

    create frame cart

--- src/components/cart.js
+++ src/components/cart.js
@@ -36,7 +36,7 @@ class Cart extends Component {
           <TouchableOpacity
             onPress={() => this.RBSheet.open()}
             style={{
-              left: '50%',
+              left: '45%',
               top: -12.5,
               position: 'absolute',
               borderRadius: 100,
@@ -62,14 +62,42 @@ class Cart extends Component {
           ref={ref => {
             this.RBSheet = ref;
           }}
-          height={500}
+          closeOnDragDown={true}
+          height={400}
           duration={250}
           customStyles={{
             container: {
-              alignItems: 'center',
+              borderTopWidth: 2,
+              borderTopColor: '#449C45',
+              elevation: 0,
             },
           }}>
-          <Text>Gila</Text>
+          <View style={{flexDirection: 'row', width: '100%', marginTop: -4}}>
+            <View style={{marginLeft: 10}}>
+              <Icon name="shopping-bag" size={16} style={{color: '#449C45'}} />
+              <Badge
+                value="2"
+                status="error"
+                containerStyle={{
+                  position: 'absolute',
+                  width: 30,
+                  left: 7,
+                  top: -7,
+                }}
+              />
+            </View>
+            <View style={{marginLeft: 30, paddingLeft: 10}}>
+              <Text style={{fontWeight: 'bold', color: '#449C45'}}>
+                240.000
+              </Text>
+            </View>
+            <View style={{marginLeft: 'auto', marginRight: 10}}>
+              <Button
+                buttonStyle={{height: 30, backgroundColor: '#449C45'}}
+                title="Checkout"
+              />
+            </View>
+          </View>
         </RBSheet>
       </>
     );
